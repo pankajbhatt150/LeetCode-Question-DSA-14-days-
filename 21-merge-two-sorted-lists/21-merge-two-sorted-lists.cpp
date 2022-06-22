6e884138077d5ec840ed700b08c9974604f19efe
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2022-06-22 11:05:47 +0530

    Time: 15 ms (31.74%), Space: 14.7 MB (81.26%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,33 +11,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       if(list1==NULL)
-        return list2;
-    else if(list2==NULL)
-        return list1;
-    
-    ListNode *dummynode = new ListNode(-1);//making a node whose next will carry the head address of list
-    ListNode *dummyhead = dummynode;//this is a pointer which will traverse on the new list
-    
-    while(list1!=NULL && list2!=NULL)
-    {
-        if(list1->val <= list2->val)
+     	if(list1 == NULL)
         {
-            dummyhead->next = list1;//if this run then first time the head address of new list will be in next of dummynode
-            list1 = list1->next;
-        }
-        else
+			return list2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(list2 == NULL)
         {
-            dummyhead->next = list2;
-            list2 = list2->next;
-        }
-        dummyhead=dummyhead->next;
-    }
-    if(list1!=NULL)
-        dummyhead->next=list1;//if list one have more nodes left then just connect head with new list
-    else
-        dummyhead->next=list2;//if list 2nd have more nodes left then just connect head with new list
-    
-       return dummynode->next;  
-    }
+			return list1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(list1 -> val <= list2 -> val)
+        {
+			list1 -> next = mergeTwoLists(list1 -> next, list2);
+			return list1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
+        {
+			list2 -> next = mergeTwoLists(list1, list2 -> next);
+			return list2;            
+		}
+	}
 };
