4960cefd4b166d7aa13f61de360bea75d08721c9
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2022-08-18 11:53:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.63%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+           if(n<2)
+            return 1;
+        int a=1,b=1,c=0;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;b=c;
+        }
+        return c;
+    }
+};
