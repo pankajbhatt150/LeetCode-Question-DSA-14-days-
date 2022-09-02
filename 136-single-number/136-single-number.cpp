a01ccef7f2d625bd9de35acdb6e60e36f4197b36
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2022-09-02 18:58:59 +0530

    Time: 21 ms (87.09%), Space: 17 MB (53.39%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      int n = nums.size(); // taking the size of the array 
+     int ans = 0;
         
-        unordered_map<int, int> mp; // unordered map to store the frequency
-        
-        // storing frequency in the map
-        for(int i = 0; i < n; i++)
-        {
-            mp[nums[i]]++;
+        for(int i=0; i<nums.size(); i++) {
+            ans = ans^nums[i];
         }
         
-        int ans; // variable to store our answer
-        for(auto x: mp) // traverse from the map
-        {
-            if(x.second == 1) //if frequency of any elemennt is 1
-            {
-                ans = x.first; // store in our answer
-                break; // break the loop, as we got our answer now
-            }
-        }
-        
-        return ans;   
-        
+        return ans;
     }
 };
