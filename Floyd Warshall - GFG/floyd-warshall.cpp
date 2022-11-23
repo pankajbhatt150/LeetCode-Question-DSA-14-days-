0c8c52375ac222a5d2ffc2cc20c4de1e08bc7202
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2022-11-23 20:02:49 +0530

    Added solution - LeetHub

---
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    // Code here
+	     int n = matrix.size();
+	    
+	    // if any path is unreachable (denoted by -1), replace it to 10^9 (infinity)
+	    // so that floyd warshal can be applied without any flaw (as it takes the minimum)
+	    for(int i = 0; i < n; ++i) {
+	        for(int j = 0; j < n; ++j) {
+	            if(matrix[i][j] == -1) {
+	                matrix[i][j] = 1e9;
+	            }
+	        }
+	    }
+	    
+	    // apply floyd warshal
+	    for(int k = 0; k < n; ++k) {
+	        for(int i = 0; i < n; ++i) {
+	            for(int j = 0; j < n; ++j) {
+	                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+	            }
+	        }
+	    }
+	    
+	    // reverse every path that is unreachable (infinity) to -1 (as question asks for)
+	    for(int i = 0; i < n; ++i) {
+	        for(int j = 0; j < n; ++j) {
+	            if(matrix[i][j] == 1e9) {
+	                matrix[i][j] = -1;
+	            }
+	        }
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>matrix(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution obj;
+		obj.shortest_distance(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cout << matrix[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
