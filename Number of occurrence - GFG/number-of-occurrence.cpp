80ef83d600b50c64f1e4dc46f61d8e2701e01992
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2023-02-05 07:09:44 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	       int left=0,right=n-1;
+
+     while(left<=right){
+
+         if(arr[left]!=x) left++;
+
+         if(arr[right]!=x) right--;
+
+         if(arr[left]==x and arr[right]==x) return right-left+1;
+
+     }
+
+     return 0; 
+	    // code here
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
