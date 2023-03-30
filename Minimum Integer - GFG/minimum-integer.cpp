abb3117adb20136da90679e6e5987f2d9b1cf885
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2023-03-30 22:00:47 +0530

    Added solution - LeetHub

---
+++ Minimum Integer - GFG/minimum-integer.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimumInteger(int N, vector<int> &A) {
+       long long sum = 0;
+        for(int i = 0 ; i < N ; i++){
+            sum = sum + A[i];
+        }
+        
+        long long x = sum/N;
+
+       if( sum%N != 0  ){
+            x++;
+       }
+       
+       vector<long long> ans;
+       for(int i = 0 ; i<N ; i++){
+            if(A[i]>=x){
+                ans.push_back(A[i]);
+            }
+       }
+
+       sort(ans.begin(),ans.end());
+
+       return ans[0];  // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.minimumInteger(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
