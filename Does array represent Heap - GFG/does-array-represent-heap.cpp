caef29a6d9a9b51bdaf18bc9af1579ff6d76c465
Pankajbhatt <76560049+pankajbhatt150@users.noreply.github.com>
2023-05-12 18:57:27 +0530

    Added solution - LeetHub

---
+++ Does array represent Heap - GFG/does-array-represent-heap.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isMaxHeap(int a[], int n)
+    {
+       for(int i=0;i<n;i++)
+        {
+            if(a[i]>a[2*i + 1] && a[i]>a[2*i + 2])
+                continue;
+            else
+                return false;
+        }
+        return true; // Your code goes here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[4*n]={0};
+       for(int i =0;i<n;i++){
+           cin >> a[i];
+       }
+       Solution ob;
+       cout<<ob.isMaxHeap(a, n)<<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
